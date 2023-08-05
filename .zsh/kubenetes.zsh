9dbef3d753c8be094c951adc876326140713237e
Toshihiko Okubo <toshihiko.okubo@datachain.jp>
2023-08-05 22:19:26 +0900

    update

--- .zsh/kubenetes.zsh
+++ .zsh/kubenetes.zsh
@@ -7,3 +7,23 @@ if command -v helm 1>/dev/null 2>&1
 then
     source <(helm completion zsh)
 fi
+
+if command -v minikube 1>/dev/null 2>&1
+then
+    source <(minikube completion zsh)
+fi
+
+if command -v kind 1>/dev/null 2>&1
+then
+    source <(kind completion zsh)
+fi
+
+if command -v argo 1>/dev/null 2>&1
+then
+    source <(argo completion zsh)
+fi
+
+if command -v argocd 1>/dev/null 2>&1
+then
+    source <(argocd completion zsh)
+fi
