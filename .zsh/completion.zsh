7deb41893105301c7ea98a95abd58c317a720098
Toshihiko Okubo <toshihiko.okubo@datachain.jp>
2024-12-13 12:51:08 +0900

    add rustup & cargo completions

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -1,15 +1,15 @@
-if type brew &>/dev/null
+if command -v brew &>/dev/null
 then
   FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-
   autoload -Uz compinit
   compinit
 fi
 
-
-# mkdir -p ~/.zsh/completions
-# cd !$
-# curl -sLO 'https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion'
+if command -v rustc >/dev/null 2>&1; then
+  FPATH="$HOME/.zfunc:${FPATH}"
+  autoload -Uz compinit
+  compinit
+fi
 
 autoload -U +X bashcompinit && bashcompinit
 if [ -f "$HOME/.zsh/completions/az.completion" ]; then
